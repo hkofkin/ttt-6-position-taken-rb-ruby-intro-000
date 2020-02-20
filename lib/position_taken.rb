9b7c133fd6db7c0259c999dd6ddf8a60e5322bd6
Hannah Kofkin <hkofkin@gmail.com>
2020-02-20 08:18:39 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,4 @@
 # code your #position_taken? method here!
-index = "X"
-user_input = gets.strip
-index_number = user_input.to_i - 1
-
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
